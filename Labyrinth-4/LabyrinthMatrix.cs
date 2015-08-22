743ce29c5c9850480976c16df0e26c283a7b3e2b
Ivaylo Andonov <ivailo.kolev.andonov@gmail.com>
2015-08-22 12:41:58 +0300

    Refactured class LabirinthMatrix
    - constants are added, created and exported method CreateMatrix with
    used constants

--- Labyrinth-4/LabyrinthMatrix.cs
+++ Labyrinth-4/LabyrinthMatrix.cs
@@ -4,6 +4,11 @@
 
     public class LabyrinthMatrix
     {
+        public const int StartPositionVertical = 3;
+        public const int StartPositionHorizontal = 3;
+        public const int MatrixRows = 7;
+        public const int MatrixCols = 7;
+
         private char[][] matrix;
         private int myPostionVertical;
         private int myPostionHorizontal;
@@ -11,24 +16,9 @@
 
         public LabyrinthMatrix()
         {
-            this.myPostionHorizontal = 3;
-            this.myPostionVertical = 3;
-            this.matrix = new char[7][];
-
-            for (int i = 0; i < this.matrix.Length; i++)
-            {
-                this.matrix[i] = new char[7];
-            }
-
-            for (int i = 0; i < this.matrix.Length; i++)
-            {
-                for (int j = 0; j < this.matrix[i].Length; j++)
-                {
-                    this.matrix[i][j] = this.GetRandomSymbol();
-                }
-            }
-
-            this.matrix[3][3] = '-';
+            this.myPostionHorizontal = StartPositionVertical;
+            this.myPostionVertical = StartPositionHorizontal;
+            this.matrix = this.CreateMatrix();
         }
 
         public LabyrinthMatrix(LabyrinthMatrix labirinth)
@@ -67,6 +57,28 @@
             {
                 this.myPostionHorizontal = value;
             }
+        }
+
+        private char[][] CreateMatrix()
+        {
+            var matrix = new char[MatrixRows][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new char[MatrixCols];
+            }
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = this.GetRandomSymbol();
+                }
+            }
+
+            matrix[3][3] = '-';
+
+            return matrix;
         }
 
         private char GetRandomSymbol()
