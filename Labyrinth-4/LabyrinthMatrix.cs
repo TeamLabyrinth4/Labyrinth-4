e8b0ea04600ed1b258e33a5f3ba82f58a6aee2a8
Ivaylo Andonov <ivailo.kolev.andonov@gmail.com>
2015-08-22 12:34:11 +0300

    LabirinthMatrix class clear and formated with StyleCop.
    - 0 wrongs, ordered usings, contructors, methods and properties. Clear
    blank lines, added this. where is needed

--- Labyrinth-4/LabyrinthMatrix.cs
+++ Labyrinth-4/LabyrinthMatrix.cs
@@ -1,87 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Labyrinth
+﻿namespace Labyrinth
 {
+    using System;
+
     public class LabyrinthMatrix
     {
         private char[][] matrix;
         private int myPostionVertical;
         private int myPostionHorizontal;
         private Random random = new Random();
+
         public LabyrinthMatrix()
         {
-            myPostionHorizontal = 3;
-            myPostionVertical = 3;
-            matrix = new char[7][];
+            this.myPostionHorizontal = 3;
+            this.myPostionVertical = 3;
+            this.matrix = new char[7][];
 
-
-
-            for (int i = 0; i <matrix.Length; i++)
+            for (int i = 0; i < this.matrix.Length; i++)
             {
-                matrix[i] = new char[7];
+                this.matrix[i] = new char[7];
             }
-            for (int i = 0; i < matrix.Length; i++)
+
+            for (int i = 0; i < this.matrix.Length; i++)
             {
-                for (int j = 0; j < matrix[i].Length; j++)
+                for (int j = 0; j < this.matrix[i].Length; j++)
                 {
-                    matrix[i][j] = GetRandomSymbol();
+                    this.matrix[i][j] = this.GetRandomSymbol();
                 }
             }
 
-            matrix[3][3] = '-';
-        }
-        public LabyrinthMatrix(LabyrinthMatrix l)
-        {
-
+            this.matrix[3][3] = '-';
         }
 
-        private char GetRandomSymbol() 
+        public LabyrinthMatrix(LabyrinthMatrix labirinth)
         {
-           
-            int random1 = random.Next(0,2);
+        }
+
+        public char[][] Matrix
+        {
+            get
+            {
+                return this.matrix;
+            }
+        }
+
+        public int MyPostionVertical
+        {
+            get
+            {
+                return this.myPostionVertical;
+            }
+
+            set
+            {
+                this.myPostionVertical = value;
+            }
+        }
+
+        public int MyPostionHorizontal
+        {
+            get
+            {
+                return this.myPostionHorizontal;
+            }
+
+            set
+            {
+                this.myPostionHorizontal = value;
+            }
+        }
+
+        private char GetRandomSymbol()
+        {
+            int random1 = this.random.Next(0, 2);
+
             if (random1 == 1)
             {
-
-
                 return 'X';
             }
             else
             {
                 return '-';
-            }
-        }
-        public char[][] Matrix 
-        {
-            get 
-            {
-                return matrix;
-            }
-        }
-        public int MyPostionVertical 
-        {
-            get 
-            {
-                return this.myPostionVertical;
-            }
-            set 
-
-
-            {
-                this.myPostionVertical = value;
-            }
-        }
-        public int MyPostionHorizontal 
-        {
-            get 
-            {
-                return this.myPostionHorizontal;
-            }
-            set 
-            {
-                this.myPostionHorizontal = value;
             }
         }
     }
