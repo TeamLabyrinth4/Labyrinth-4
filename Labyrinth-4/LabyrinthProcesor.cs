39aa7794c79a0e751c722cbc22470624b50246da
emilti <emilti1986@gmail.com>
2015-08-22 16:23:51 +0300

    Regions are removed, added constants, added key word this where necessary, the usings are moved inside the namespace, reordering the property LabyrinthMatrix - moved after constructor

--- Labyrinth-4/LabyrinthProcesor.cs
+++ Labyrinth-4/LabyrinthProcesor.cs
@@ -1,79 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Labyrinth
+﻿namespace Labyrinth
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public class LabyrinthProcesor
     {
-        #region Fields
+        public const int MaximalHorizontalPosition = 6;
+        public const int MinimalHorizontalPosition = 0;
+        public const int MaximalVerticalPosition = 6;
+        public const int MinimalVerticalPosition = 0;
 
         private LabyrinthMatrix matrix;
         private uint moveCount;
-        private Top5Scoreboard scoreboard;
-
-        #endregion
-
-        #region Properties
-
-        public LabyrinthMatrix Matrix
-        {
-            get { return matrix; }
-            set { matrix = value; }
-        }
-
-        #endregion
+        private Top5Scoreboard scoreboard;       
 
         public LabyrinthProcesor()
         {
-            scoreboard = new Top5Scoreboard();
-            Restart();
+            this.scoreboard = new Top5Scoreboard();
+            this.Restart();
         }
+
+        public LabyrinthMatrix Matrix
+        {
+            get { return this.matrix; }
+            set { this.matrix = value; }
+        }
+
         public void ShowInputMessage()
         {
             Console.Write("Enter your move (L=left, R-right, U=up, D=down): ");
         }
-        public void HandleInput(String input)
-        {
-            String lowerInput = input.ToLower();
 
-            switch(lowerInput)
+        public void HandleInput(string input)
+        {
+            string lowerInput = input.ToLower();
+
+            switch (lowerInput)
             {
                 case "l":
-                    if (!moveLeft())
+                    if (!this.MoveLeft())
                     {
                         Console.WriteLine("Invalid move!");
                     }
+
                 break;
 
                 case "r":
-                    if (!moveRight())
+                    if (!this.MoveRight())
                     {
                         Console.WriteLine("Invalid move!");
                     }
+
                 break;
 
                 case "u":
-                    if (!moveUp())
+                    if (!this.MoveUp())
                     {
                         Console.WriteLine("Invalid move!");
                     }
+
                 break;
 
                 case "d":
-                    if (!moveDown())
+                    if (!this.MoveDown())
                     {
                         Console.WriteLine("Invalid move!");
                     }
+
                 break;
 
                 case "top":
-                    scoreboard.ShowScoreboard();
+                    this.scoreboard.ShowScoreboard();
                 break;
 
                 case "restart":
-                    Restart();
+                    this.Restart();
                 break;
 
                 case "exit":
@@ -86,80 +89,81 @@ namespace Labyrinth
                 break;
             }
 
-            IsFinished();
+            this.IsFinished();
         }
+
         private void IsFinished()
         {
-            if (matrix.MyPostionHorizontal == 0 ||
-                matrix.MyPostionHorizontal == 6 ||
-                matrix.MyPostionVertical == 0 ||
-                matrix.MyPostionVertical == 6)
+            if (this.matrix.MyPostionHorizontal == MinimalHorizontalPosition ||
+                this.matrix.MyPostionHorizontal == MaximalHorizontalPosition ||
+                this.matrix.MyPostionVertical == MinimalVerticalPosition ||
+                this.matrix.MyPostionVertical == MaximalVerticalPosition)
             {
-                Console.WriteLine("Congratulations! You escaped in " + moveCount.ToString() + " moves.");
-                scoreboard.HandleScoreboard(moveCount);
-                Restart();
+                Console.WriteLine("Congratulations! You escaped in " + this.moveCount.ToString() + " moves.");
+                this.scoreboard.HandleScoreboard(this.moveCount);
+                this.Restart();
             }
         }
+
         private void Restart()
         {
             Console.WriteLine();
             Console.WriteLine("Welcome to “Labirinth” game. Please try to escape. Use 'top' to view the top scoreboard, 'restart' to start a new game and 'exit' to quit the game.");
 
             this.matrix = new LabyrinthMatrix();
-            moveCount = 0;
+            this.moveCount = 0;
         }
-        #region Move Methods
 
-        private bool moveDown()
+        private bool MoveDown()
         {
-            if (!(matrix.MyPostionVertical == 6) &&
-                this.matrix.Matrix[matrix.MyPostionHorizontal][matrix.MyPostionVertical + 1] == '-')
+            if (!(this.matrix.MyPostionVertical == MaximalVerticalPosition) &&
+                this.matrix.Matrix[this.matrix.MyPostionHorizontal][this.matrix.MyPostionVertical + 1] == '-')
             {
-                matrix.MyPostionVertical++;
-                moveCount++;
+                this.matrix.MyPostionVertical++;
+                this.moveCount++;
                 return true;
             }
 
             return false;
         }
 
-        private bool moveUp()
+        private bool MoveUp()
         {
-            if (!(matrix.MyPostionVertical == 0) &&
-                this.matrix.Matrix[matrix.MyPostionHorizontal][matrix.MyPostionVertical - 1] == '-')
+            if (!(this.matrix.MyPostionVertical == MinimalVerticalPosition) &&
+                this.matrix.Matrix[this.matrix.MyPostionHorizontal][this.matrix.MyPostionVertical - 1] == '-')
             {
-                matrix.MyPostionVertical--;
-                moveCount++;
-                return true;
-            }
-
-            return false;
-        }
-        private bool moveRight()
-        {
-            if (!(matrix.MyPostionHorizontal == 6) &&
-                 this.matrix.Matrix[matrix.MyPostionHorizontal+ 1][matrix.MyPostionVertical] == '-')
-            {
-                matrix.MyPostionHorizontal++;
-                moveCount++;
-                return true;
-            }
-
-            return false;
-        }
-        private bool moveLeft()
-        {
-            if (!(matrix.MyPostionHorizontal == 0) &&
-                this.matrix.Matrix[matrix.MyPostionHorizontal - 1][matrix.MyPostionVertical] == '-')
-            {
-                matrix.MyPostionHorizontal--;
-                moveCount++;
+                this.matrix.MyPostionVertical--;
+                this.moveCount++;
                 return true;
             }
 
             return false;
         }
 
-        #endregion
+        private bool MoveRight()
+        {
+            if (!(this.matrix.MyPostionHorizontal == MaximalHorizontalPosition) &&
+                 this.matrix.Matrix[this.matrix.MyPostionHorizontal + 1][this.matrix.MyPostionVertical] == '-')
+            {
+                this.matrix.MyPostionHorizontal++;
+                this.moveCount++;
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool MoveLeft()
+        {
+            if (!(this.matrix.MyPostionHorizontal == MinimalHorizontalPosition) &&
+                this.matrix.Matrix[this.matrix.MyPostionHorizontal - 1][this.matrix.MyPostionVertical] == '-')
+            {
+                this.matrix.MyPostionHorizontal--;
+                this.moveCount++;
+                return true;
+            }
+
+            return false;
+        }
     }
 }
