2b547d8f483078da16c1e2186600e4c282c48fe0
Chavdar Angelov <c.l.angelov@gmail.com>
2015-09-15 10:10:58 +0300

    Launching the application from separate file

---
+++ Labyrinth-4/AppStart.cs
@@ -0,0 +1,21 @@
+﻿namespace Labyrinth
+{
+    using System;
+
+    public static class AppStart
+    {
+        public static void Main()
+        {
+            LabyrinthProcesor processor = new LabyrinthProcesor();
+
+            while (true)
+            {
+                ConsoleWritter.ShowLabyrinth(processor.Matrix);
+                processor.ShowInputMessage();
+                string input;
+                input = Console.ReadLine();
+                processor.HandleInput(input);
+            }
+        }
+    }
+}
---
+++ Labyrinth-4/ConsoleWritter.cs
@@ -0,0 +1,30 @@
+﻿namespace Labyrinth
+{
+    using System;
+
+    public class ConsoleWritter
+    {
+        // BIG Issue this must NOT BE STATIC
+        public static void ShowLabyrinth(LabyrinthMatrix labyrinth)
+        {
+            Console.WriteLine();
+            char[][] myMatrix = labyrinth.Matrix;
+            for (int i = 0; i < myMatrix.Length; i++)
+            {
+                for (int j = 0; j < myMatrix[i].Length; j++)
+                {
+                    if (i == labyrinth.MyPostionVertical && j == labyrinth.MyPostionHorizontal)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(myMatrix[j][i]);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }       
+    }
+}
