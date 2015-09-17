cc2fc0f73bf8861e9f4020c745dd5831df3477b6
Chavdar Angelov <c.l.angelov@gmail.com>
2015-09-17 15:38:46 +0300

    removed unneeded files and using System's

--- Labyrinth-4/Renderer/ConsoleRenderer.cs
+++ Labyrinth-4/Renderer/ConsoleRenderer.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Labyrinth.Renderer
+﻿namespace Labyrinth.Renderer
 {
+    using System;
+
     public class ConsoleRenderer : IRenderer
     {
         public ConsoleRenderer()
--- Labyrinth-4/Renderer/IRenderer.cs
+++ Labyrinth-4/Renderer/IRenderer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Labyrinth.Renderer
+﻿namespace Labyrinth.Renderer
 {
     public interface IRenderer
     {
