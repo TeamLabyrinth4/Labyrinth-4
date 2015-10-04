7f6ab2b84d60321d98dc5cf0aecde60afe76abe8
Chavdar Angelov <c.l.angelov@gmail.com>
2015-10-04 17:53:00 +0300

    Hot fix for Builder Pattern

--- Labyrinth-4/ObjectBuilder/GameConstructor.cs
+++ Labyrinth-4/ObjectBuilder/GameConstructor.cs
@@ -10,16 +10,12 @@
         private IScoreBoardObserver scoreBoardHandler;
         private LabyrinthProcesor procesor;
 
-        public void SetupGame(SimpleConsoleGameBuilder objectBuilder)
+        public Game SetupGame(SimpleConsoleGameBuilder objectBuilder)
         {
             this.renderer = objectBuilder.CreteRenderer();
             this.player = objectBuilder.CreatePlayer();
             this.scoreBoardHandler = objectBuilder.CreteScoreBoardHanler();
             this.procesor = new LabyrinthProcesor(this.renderer, this.player, this.scoreBoardHandler);
-        }
-
-        public Game InitGame()
-        {
             return Game.Instance(this.player, this.renderer, this.scoreBoardHandler, this.procesor);
         }
     }
