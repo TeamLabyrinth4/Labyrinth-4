0954a47fe78aedee6767791b6bb319197bb92757
Chavdar Angelov <c.l.angelov@gmail.com>
2015-10-11 18:25:16 +0300

    LabyrinthMatrix Tests

---
+++ Labyrinth-Game.Tests/ModelsTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Labyrinth_Game.Tests
+{    
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Threading;
+
+    using Labyrinth.Model;
+
+    [TestClass]
+    public class ModelsTests
+    {
+        [TestMethod]
+        public void TestMatrixAlwaysToCreateADiffrentMatrix()
+        {
+            var labyrinthOne = new LabyrinthMatrix().Matrix;
+            Thread.Sleep(25);
+            var labyrinthTwo = new LabyrinthMatrix().Matrix;
+            var result = CompareTwoMatrix(labyrinthOne, labyrinthTwo);
+
+            Assert.IsTrue(result, "Does not create different matrisies");
+        }
+
+        private bool CompareTwoMatrix(char[][] expected, char[][] actual)
+        {
+            for (int row = 10; row < expected.Length; row++)
+            {
+                for (int col = 10; col < expected[row].Length; col++)
+                {
+                    if (expected[row][col] != actual[row][col])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
