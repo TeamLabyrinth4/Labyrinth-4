12f8313270452d5eb092d0f72a399181228e2cba
tcholakov <tcholakov2@gmail.com>
2015-10-11 13:38:37 +0300

    Added Unit test and Moq framework

---
+++ Labyrinth-Game.Tests/GameEngineInstanceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Labyrinth_Game.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Labyrinth.Users;
+    using Labyrinth.Renderer;
+    using Labyrinth.Scoreboard;
+    using Labyrinth.Model;
+    using Labyrinth.Utilities;
+    using Labyrinth;
+
+    [TestClass]
+    public class GameEngineInstanceTests
+    {
+        [TestMethod]
+        public void IntanceReturnsGameEngineObject()
+        {
+            var mockedPlayer = new Mock<IPlayer>();
+            var mockedRenderer = new Mock<IRenderer>();
+            var mockedScoreBoardObserver = new Mock<IScoreBoardObserver>();
+            var mockedLabyrinthMatrix = new LabyrinthMatrix();
+            var mockedMessages = new Messages();
+
+            var game = GameEngine.Instance(mockedPlayer.Object, mockedRenderer.Object, mockedScoreBoardObserver.Object, mockedLabyrinthMatrix, mockedMessages);
+
+            Assert.IsTrue(game is GameEngine);
+        }
+    }
+}
