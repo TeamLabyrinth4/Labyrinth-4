f501a7535232f9267ac150ae87a9f6cbc68eef5f
emilti <emilti1986@gmail.com>
2015-10-12 13:33:38 +0300

    SimpleConsoleObjectBuilderTests

---
+++ Labyrinth-Game.Tests/GameBuilderTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Labyrinth.Model;
+using Labyrinth.ObjectBuilder;
+using Labyrinth.Renderer;
+using Labyrinth.Scoreboard;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace Labyrinth_Game.Tests
+{
+    [TestClass]
+    public class GameBuilderTests
+    {
+        [TestMethod]
+        public void TestMethodCreateRendererToReturnConsoleRenderer()
+        {
+            var simpleConsoleGameBuilder = new SimpleConsoleGameBuilder();
+            var renderer = simpleConsoleGameBuilder.CreteRenderer();
+            Assert.IsTrue(renderer.GetType() == typeof(ConsoleRenderer));
+        }
+
+        [TestMethod]
+        public void TestCreateLabyrinthMatrixToReturnLabyrintMatrix()
+        {
+            var simpleConsoleGameBuilder = new SimpleConsoleGameBuilder();
+            var labyrinthMatrix = simpleConsoleGameBuilder.CreateLabyrinthMatrix();
+            Assert.IsTrue(labyrinthMatrix.GetType() == typeof(LabyrinthMatrix));
+        }
+
+        [TestMethod]
+        public void TestCreateScoreboardToReturnScoreboard()
+        {
+            var simpleConsoleGameBuilder = new SimpleConsoleGameBuilder();
+            var scoreboard = simpleConsoleGameBuilder.CreateScoreboard();
+            Assert.IsTrue(scoreboard.GetType() == typeof(LocalScoreBoard));
+        }
+
+        // [TestMethod]
+        // public void TestGetUsernameToReturnUsername()
+        // {
+        //     var simpleConsoleGameBuilder = new SimpleConsoleGameBuilder();           
+        //     Assert.IsTrue(username.GetType() == typeof(string));
+        // }
+    }
+}
